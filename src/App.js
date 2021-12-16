ba83830c644b6f4b12d4c542310e569992733c54
Silvia Jesus <jssilviar@gmail.com>
2021-12-16 11:59:25 -0500

    desafio clase 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import ladyLogo from './logoText.png';
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ladyLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
