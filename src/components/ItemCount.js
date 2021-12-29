bfb3220f4e020b02f343676c2d60fb670cfd2e3a
Daniel André Ávila Quispe <avilaqdaniel@gmail.com>
2021-12-28 23:27:12 -0500

    desafio clase 5 : creando un boton

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,16 +1,23 @@
 import {useState} from 'react'
 
-export function ItemCount( stock , initial , onAdd){
-    const [count, setCount] = useState(1);
-
-    const resultadoBoton = () => {
-        alert(`Stock: ${stock}
-                Count: ${count}
-                validarStock()`)
-    }
+//las props se reciben como propiedades de un objeto {}, asi pueden pasarse dentro del hook 'useState'
+export function ItemCount({stock , initial}){
+    const [count, setCount] = useState(initial);
 
     const validarStock =() => {
         return stock >= count
+    }
+
+    const maxStock =() => {
+        return stock > count
+    }
+
+    function onAdd () {
+        alert(`¡Genial! Añadiste ${count} unidades a tu carrito.`)
+    }
+    
+    function minStock () {
+        return count >= 2;
     }
 
     return (
@@ -20,14 +27,17 @@ export function ItemCount( stock , initial , onAdd){
                     return prevCount -1
                 })
             }}
+            disabled={!minStock()}
             > - </button>
             <span className="px-4 text-2xl">{count}</span>
             <button className="rounded px-4 py-2" onClick={()=>{
                 setCount((prevCount) =>{
                     return prevCount +1
                 })
-            }}> + </button>
-            <button className="bg-yellow-400" onClick={resultadoBoton}
+            }}
+            disabled={!maxStock()}
+            > + </button>
+            <button className="bg-warning" onClick={onAdd} disabled={!validarStock()}
             >Agregar al carrito</button>
         </div>
     )
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,5 +2,6 @@ import { ItemCount} from './ItemCount'
 
 export function ItemListContainer (){
 
+    //Las props es mejor pasarlas dentro de llaves, porque si no se toman por strings dentro del componente
     return <ItemCount stock={15} initial={1}/>
 }
