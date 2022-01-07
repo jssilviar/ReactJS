947526804927e160e57573123094a11d4874aae3
Silvia Jesus <jssilviar@gmail.com>
2022-01-07 13:29:02 -0500

    ASe añade gif loader

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,21 @@
 import {Item} from './Item'
+import Pacman from '../pacman_loading.svg'
 
 export function ItemList({cakes}){
-    return(
-        <div className='cards container' >
-            {cakes.map(itemCakes => {
-                return <Item key={itemCakes.id} productCake={itemCakes} />
-            })}
-        </div>
-    )
+    if (cakes.length === 0) {
+        return (
+            <>
+                <img className='' src={Pacman} alt='Pacman'/>
+                Cargando
+            </>
+        )  
+    } else {
+        return(
+            <div className='cards container' >
+                {cakes.map(itemCakes => {
+                    return <Item key={itemCakes.id} productCake={itemCakes} />
+                })}
+            </div>
+        )
+    }
 }
