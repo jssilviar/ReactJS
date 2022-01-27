cfd6f1f4da8515c0c88dd61c9a2d876da28204ca
Silvia Jesus <jssilviar@gmail.com>
2022-01-27 11:44:14 -0500

    modificando el product.json

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAs7A-Jn7hSy1eMfT38Ti8ISUte1WyvNlE",
+  authDomain: "ladydicakes-f8ed1.firebaseapp.com",
+  projectId: "ladydicakes-f8ed1",
+  storageBucket: "ladydicakes-f8ed1.appspot.com",
+  messagingSenderId: "420834755829",
+  appId: "1:420834755829:web:4e8bbde4ce7f020bbbf1bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
